8ce90f039e76330b676d806816991e9e659f2018
Rachel McGrane <rmcgrane@forever.com>
2017-10-23 15:56:48 -0400

    Renamed initializer to match module name

--- lib/forever_style_guide/engine.rb
+++ lib/forever_style_guide/engine.rb
@@ -1,6 +1,6 @@
 module ForeverStyleGuide
   class Engine < Rails::Engine
-    initializer 'hello_world.assets.precompile' do |app|
+    initializer 'forever_style_guide.assets.precompile' do |app|
       %w(stylesheets).each do |sub|
         app.config.assets.paths << root.join('assets', sub).to_s
       end
